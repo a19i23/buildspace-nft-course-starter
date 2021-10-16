70e926a39b1beb36bea4acfb23fa0baf2417574d
Alan Villa <a19i23@hotmail.com>
2021-10-15 21:46:26 -0700

    adding event listener

--- src/App.js
+++ src/App.js
@@ -9,7 +9,9 @@ import myEpicNft from './utils/MyEpicNFT.json';
 const TWITTER_HANDLE = '_buildspace';
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 // const OPENSEA_LINK = '';
-// const TOTAL_MINT_COUNT = 50;
+const TOTAL_MINT_COUNT = 50;
+const CONTRACT_ADDRESS = "0x18E08d8cBD0D848C6a625E8cF8F84b3eDA7B5aB9";
+
 
 const App = () => {
 
@@ -43,6 +45,11 @@ const App = () => {
           const account = accounts[0];
           console.log("Found an authorized account:", account);
           setCurrentAccount(account)
+
+
+          // Setup listener! This is for the case where a user comes to our site
+          // and ALREADY had their wallet connected + authorized.
+          setupEventListener()
       } else {
           console.log("No authorized account found")
       }
@@ -70,13 +77,47 @@ const App = () => {
       */
       console.log("Connected", accounts[0]);
       setCurrentAccount(accounts[0]); 
+
+    
+      // Setup listener! This is for the case where a user comes to our site
+      // and connected their wallet for the first time.
+      setupEventListener() 
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+   // Setup our listener.
+  const setupEventListener = async () => {
+    // Most of this looks the same as our function askContractToMintNft
+    try {
+      const { ethereum } = window;
+
+      if (ethereum) {
+        // Same stuff again
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const connectedContract = new ethers.Contract(CONTRACT_ADDRESS, myEpicNft.abi, signer);
+
+        // THIS IS THE MAGIC SAUCE.
+        // This will essentially "capture" our event when our contract throws it.
+        // If you're familiar with webhooks, it's very similar to that!
+        connectedContract.on("NewEpicNFTMinted", (from, tokenId) => {
+          console.log(from, tokenId.toNumber())
+          alert(`Hey there! We've minted your NFT and sent it to your wallet. It may be blank right now. It can take a max of 10 min to show up on OpenSea. Here's the link: https://testnets.opensea.io/assets/${CONTRACT_ADDRESS}/${tokenId.toNumber()}`)
+        });
+
+        console.log("Setup event listener!")
+
+      } else {
+        console.log("Ethereum object doesn't exist!");
+      }
     } catch (error) {
       console.log(error)
     }
   }
 
   const askContractToMintNft = async () => {
-  const CONTRACT_ADDRESS = "0x18E08d8cBD0D848C6a625E8cF8F84b3eDA7B5aB9";
     try {
       const { ethereum } = window;
 
@@ -133,6 +174,9 @@ const App = () => {
               Mint NFT
             </button>
           )}
+           {/* <p className="sub-text">
+            /{TOTAL_MINT_COUNT} NFTs minted so far
+          </p> */}
         </div>
         <div className="footer-container">
           <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
